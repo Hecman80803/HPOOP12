0506e0e016725901efbab30cbd7f988a99d008fc
Hecman80803 <147789917+Hecman80803@users.noreply.github.com>
2023-11-23 22:29:00 -0600

    Adición del Javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"poop12","l":"Cuenta"},{"p":"poop12","l":"HiloCeros"},{"p":"poop12","l":"HiloR"},{"p":"poop12","l":"HilosSaltoDeLinea"},{"p":"poop12","l":"HiloT"},{"p":"poop12","l":"HiloUnos"},{"p":"poop12","l":"POOP12"}]
